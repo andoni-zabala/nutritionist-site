baf1c9c5671e2aa681b71b440ae62ec640838f72
Andoni Zabala <andonifzabala@gmail.com>
2026-04-18 10:45:22 -0300

    seo improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://licmabelgomez.netlify.app/',
+  integrations: [
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.9,
+      lastmod: new Date(),
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
